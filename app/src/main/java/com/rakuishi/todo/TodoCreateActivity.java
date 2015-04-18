5fd3e3bb5c02a0e31014526a7abe056b8da199f4
rakuishi <rakuishi@gmail.com>
2015-04-18 19:16:08 +0900

    Add TodoCreateActivity

---
+++ app/src/main/java/com/rakuishi/todo/TodoCreateActivity.java
@@ -0,0 +1,16 @@
+package com.rakuishi.todo;
+
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+
+/**
+ * Created by rakuishi on 15/04/18.
+ */
+public class TodoCreateActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.todo_create);
+    }
+}
