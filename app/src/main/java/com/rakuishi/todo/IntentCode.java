7b716bcc95e8878283a6782e5785de78cbeb88c2
rakuishi <rakuishi@gmail.com>
2015-04-18 19:30:51 +0900

    Add `createIntent` methods

---
+++ app/src/main/java/com/rakuishi/todo/IntentCode.java
@@ -0,0 +1,11 @@
+package com.rakuishi.todo;
+
+/**
+ * Created by rakuishi on 15/04/18.
+ */
+public class IntentCode {
+
+    public static final String INTENT_PREFIX = "com.rakuishi.todo.";
+    public static final String INTENT_EXTRA_PREFIX = INTENT_PREFIX + "extra.";
+    public static final String EXTRA_ID = INTENT_EXTRA_PREFIX + "ID";
+}
--- app/src/main/java/com/rakuishi/todo/TodoCreateActivity.java
+++ app/src/main/java/com/rakuishi/todo/TodoCreateActivity.java
@@ -1,5 +1,8 @@
 package com.rakuishi.todo;
 
+import static com.rakuishi.todo.IntentCode.EXTRA_ID;
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 
@@ -7,6 +10,16 @@ import android.support.v7.app.ActionBarActivity;
  * Created by rakuishi on 15/04/18.
  */
 public class TodoCreateActivity extends ActionBarActivity {
+
+    public static Intent createIntent(Context context) {
+        return new Intent(context, TodoCreateActivity.class);
+    }
+
+    public static Intent createIntent(Context context, int id) {
+        Intent intent = new Intent(context, TodoCreateActivity.class);
+        intent.putExtra(EXTRA_ID, id);
+        return intent;
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
